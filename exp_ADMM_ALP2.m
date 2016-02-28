050e86f48e3fe6d180b8ff9d918f90347dce9191
kickooyoo <mtle@umich.edu>
2016-02-28 12:45:25 -0500

    no mask used in NRMSE, smpd uses only 4 workers

--- exp_ADMM_ALP2.m
+++ exp_ADMM_ALP2.m
@@ -15,7 +15,7 @@ end
 %[xhat_tri_max, ~, nrmsd_tri_max, costOrig_tri_max, time_tri_max] = ADMM_tridiag(y_noise, F, S, CH, CV, beta, xinit, x_tri_inf, niters, 'mask', mask, 'nthread', int32(maxNumCompThreads('automatic')),'mu', ones(1,5));
 [xhat_tri_FP, ~, nrmsd_tri_FP, costOrig_tri_FP, time_tri_FP] = ADMM_FP_tridiag(y_noise, F, S, CH, CV, beta, xinit, x_tri_inf, niters, 'mask', mask);
 
-save(sprintf('./reviv/curr/%s_timing_%dx%d_%diter_%s_ADMMtrue.mat', machine(1:3), Nx, Ny, niters, slice_str));
-send_mai_text('done with mpel8 timing');
+save(sprintf('./reviv/curr/%s_timing_%dx%d_%diter_%s_MFISTAtrue.mat', machine(1:3), Nx, Ny, niters, slice_str));
+%send_mai_text('done with mpel8 timing');
 
 display('DONE');
