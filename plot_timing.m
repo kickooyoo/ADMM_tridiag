5d7eda36a56297f177f096782ae0f8374ab9f978
kickooyoo <kickooyoo@gmail.com>
2016-04-21 18:10:08 -0400

    better plotting for paper

--- plot_timing.m
+++ plot_timing.m
@@ -1,10 +1,13 @@
-colors = 'cmgkbry';
-markers = 'ox+sd*.^v><ph';
-<<<<<<< HEAD
-% markers = '-';
-=======
-%markers = '-';
->>>>>>> 30b803c31ccfabd8a6f906123705ca3e682028a9
+if ~isvar('colors')
+        colors = 'cmgkbry';
+end
+if ~isvar('markers')
+        markers = 'xo+*sd.^v><ph';
+end
+if ~isvar('ystr')
+        ystr = 'NRMSD to x^{(\infty)} (dB)';
+end
+MarkerSize = 8;
 exps = who('time*');
 start_ndx = 1;
 end_ndx = niters+1;
@@ -16,13 +19,16 @@ xform = @(x) 20*log10(x);
 if ~isvar('FontSize')
 	FontSize = 12;
 end
-
+if ~isvar('order')
+        order = 1:length(exps);
+end
 y_val = 'nrmsd'; %'nrmsd'; %'costOrig'
 xlabels = {'wall time (s)'; 'iteration number'};
 for plot_ndx = 1:2
 fhandle = figure('Position', [100 100 400 300]); hold on; 
 legend_str = {};
-for ii = 1:length(exps)
+for kk = 1:length(exps)
+        ii = order(kk);
 	curr_name = exps{ii};
 	suffix_ndx = strfind(curr_name, '_');
 	suffix = curr_name(suffix_ndx : end);
@@ -43,12 +49,12 @@ for ii = 1:length(exps)
         if start_ndx == 1
                 tcomp = sprintf(' - %s(1)', curr_name);
         end
-	curr_color = colors(mod(ii, length(colors)) + 1);
+	curr_color = colors(mod(kk, length(colors)) + 1);
 	for jj = 1:Nd
                 if plot_ndx == 1
-                        plot_command = sprintf('h(ii,1) = plot(cumsum(%s(%s))%s, xform(%s%s(%s)), ''%s%s'');', curr_name, indeces, tcomp, y_val, suffix, indeces, curr_color, '-');
+                        plot_command = sprintf('h(kk,1) = plot(cumsum(%s(%s))%s, xform(%s%s(%s)), ''%s%s'');', curr_name, indeces, tcomp, y_val, suffix, indeces, curr_color, '-');
                 else
-                        plot_command = sprintf('h(ii,1) = plot(xform(%s%s(%s)), ''%s%s'');', y_val, suffix, indeces, curr_color, '-');
+                        plot_command = sprintf('h(kk,1) = plot(xform(%s%s(%s)), ''%s%s'');', y_val, suffix, indeces, curr_color, '-');
                 end
                 try
                         eval(plot_command);
@@ -74,11 +80,11 @@ for ii = 1:length(exps)
 %         end
         % add ornaments
         for jj = 1:Nd
-		curr_marker = markers(mod(ii, length(markers)) + 1);
+		curr_marker = markers(mod(kk, length(markers)) + 1);
                 if plot_ndx == 1
-                        plot_command = sprintf('h(ii,2) = plot(sum(%s(%s))%s, xform(%s%s(%s)), ''%s%s'');', curr_name, orn_indeces, tcomp, y_val, suffix, orn_index, curr_color, curr_marker);
+                        plot_command = sprintf('h(kk,2) = plot(sum(%s(%s))%s, xform(%s%s(%s)), ''%s%s'', ''MarkerFaceColor'', ''%s'', ''MarkerSize'', MarkerSize);', curr_name, orn_indeces, tcomp, y_val, suffix, orn_index, curr_color, curr_marker, curr_color);
                 else
-                        plot_command = sprintf('h(ii,2) = plot(orn_ndx, xform(%s%s(%s)), ''%s%s'');', y_val, suffix, orn_index, curr_color, curr_marker);
+                        plot_command = sprintf('h(kk,2) = plot(orn_ndx, xform(%s%s(%s)), ''%s%s'', ''MarkerFaceColor'', ''%s'', ''MarkerSize'', MarkerSize);', y_val, suffix, orn_index, curr_color, curr_marker, curr_color);
                 end
                 try
                         eval(plot_command);
@@ -89,16 +95,19 @@ for ii = 1:length(exps)
 	end
 end
 [lh, oh] = legend(h(:,1), legend_str, 'FontSize', FontSize);
-for ii = 1:length(exps)
+% add ornaments to lines
+for kk = 1:length(exps)
+        ii = order(kk);
         oh_ndx = length(exps) + (ii - 1)*2 + 1;
         curr_marker = markers(mod(ii, length(markers)) + 1);
-        set(oh(oh_ndx), 'Marker', curr_marker);
+        curr_color = colors(mod(ii, length(colors)) + 1);
+        set(oh(oh_ndx), 'Marker', curr_marker, 'MarkerFaceColor', curr_color, 'MarkerSize', MarkerSize);
 end
 % c = get(l,'Children');
 % set(c(1),'Marker','o')
 xlabel(xlabels{plot_ndx}, 'FontSize', FontSize)
-title(sprintf('%s to x^* over time for [%d %d]', upper(y_val), Nx, Ny))
-ylabel('NRMSD to x^* (dB)', 'FontSize', FontSize);
+title(sprintf('%s to %s over time for [%d %d]', upper(y_val),'x^{(\infty)}',  Nx, Ny))
+ylabel(ystr, 'FontSize', FontSize);
 if isvar('plot_axes')
         axis(plot_axes{plot_ndx})
 else
--- toy_script.m
+++ toy_script.m
@@ -1,39 +1,48 @@
 % tests out AL_P2_gen and tridiag_ADMM on simulated brainweb data
 
-nx = 24;
-ny = 20;
-dims = [nx ny];
-nc = 4;
+Nx = 24;
+Ny = 20;
+dims = [Nx Ny];
+Nc = 4;
 
-img = make_sim_image(nx,ny);
-sense_maps = mri_sensemap_sim('nx', nx, 'ny', ny, 'ncoil', nc, 'rcoil', 3*ny);
-% samp = gen_poisson_sampling_pattern('2D',[nx ny], [floor(nx/6) floor(ny/6)],6); 
-params.Nx = nx;
-params.Ny = ny;
+img = make_sim_image(Nx, Ny);
+sense_maps = mri_sensemap_sim('nx', Nx, 'ny', Ny, 'ncoil', Nc, 'rcoil', 3*Ny);
+% samp = gen_poisson_sampling_pattern('2D',[Nx Ny], [floor(Nx/6) floor(Ny/6)],6); 
+params.Nx = Nx;
+params.Ny = Ny;
 params.Nf = 1;
 params.h = 1;
-params.R = round(nx*ny/2);
+params.R = round(Nx*Ny/4);
 samp = logical(gen_new_sampling_pattern(params, 'all_kspace', 0));
 
-imgs = repmat(img,[1 1 nc]).*sense_maps;
+imgs = repmat(img,[1 1 Nc]).*sense_maps;
 % S = construct_sensefat(sense_maps);
 S = staticS(sense_maps);
 
-% subsampling Fourier encoding matrix F
-% F = construct_fourierfat(samp,nc);
-F = staticF(nx, ny, nc, 'samp', samp);
+% subsampling Fourier eNcoding matrix F
+% F = construct_fourierfat(samp,Nc);
+F = staticF(Nx, Ny, Nc, 'samp', samp);
 
 SFFS = full(S'*F'*F*S);
+[SFeigs, ~] = get_eigs(S'*F'*F*S, 1, 'Nx', Nx, 'Ny', Ny);
+[Feigs, Q] = get_eigs(F'*F, Nc, 'Nx', Nx, 'Ny', Ny);
+[V, D] = eig(SFFS);
 %%
+Rcirc = Cdiffs([Nx Ny],'offsets', [1 Nx], 'type_diff','circshift');
+RR = full(Rcirc'*Rcirc);
+[Reigs, Q] = get_eigs(Rcirc'*Rcirc, 1, 'Nx', Nx, 'Ny', Ny);
+VRRV = V*RR*V';
+VLRV = V*diag(Reigs)*V';
+figure; im(VLRV)
 %% 
-params.N = [nx ny];
+params.N = [Nx Ny];
 params.A = F*S;
 params.W = ones(prod(F.odim), 1);
 params.eigtol = eps; % Matlab epsilon
 params.eigpowermaxitr = 10000;
 params.eigdispitr = 10;
 mEAWA = get_MaxEigenValue_1by1(params, 'AWA');
-[V, D] = eig(SFFS);
+
 return
 %% 
 
@@ -44,7 +53,7 @@ snr = 80;
 %sig = 10^(-snr/20) * norm(ytrue(samp)) / sqrt(sum(samp(:)));
 sig = 10^(-snr/20) * norm(y) / sqrt(length(y));
 noisey = y + sig*randn(size(y)) + i*sig*randn(size(y));
-% finite-differencing matrices C1 and C2
+% finite-differeNcing matrices C1 and C2
 [C1, C2] = construct_finite_diff(dims);
 
 % choose parameters
@@ -58,18 +67,18 @@ alph = 0.5;
 
 xzfill = S'*(F'*noisey);
 xzfill = ir_wls_init_scale((F*S), noisey, xzfill);
-xinit = reshape(xzfill, nx, ny);
+xinit = reshape(xzfill, Nx, Ny);
 pot = potential_fun('quad');
 
 R = [C1; C2];
 curr_folder = '.';
-[xMFIS, CMFIS, TFIS, l2DFIS, RMSEFIS] = MFISTA_wrapper(nx, ny, R, noisey, xinit, F, S, beta, niters, curr_folder);
+[xMFIS, CMFIS, TFIS, l2DFIS, RMSEFIS] = MFISTA_wrapper(Nx, Ny, R, noisey, xinit, F, S, beta, niters, curr_folder);
 
 return
 %%
 % A_bs = [sqrt(1/2)*full(F*S); sqrt(beta)*full(R)];
 % y_bs = [sqrt(1/2)*col(y); zeros(size(R*xinit(:)))];
-% xhat_bs = reshape(A_bs\y_bs, nx, ny);
+% xhat_bs = reshape(A_bs\y_bs, Nx, Ny);
 
 % ALP2
 % [xhat_P2, ~, err_P2, costOrig_P2] = AL_P2_gen_genpot(noisey, F, S, R,...
@@ -77,13 +86,13 @@ return
 
 % tridiag solver
 % [xhat_tri, xsaved_tri, cost_tri] = tridiag_ADMM_genpot(noisey, F, S, C1, C2, ...
-%         alph, beta, xinit, img, niters, 'pot', pot, 'fancy_mu', 1); 
+%         alph, beta, xinit, img, niters, 'pot', pot, 'faNcy_mu', 1); 
 
 [xhat_tri, xsaved_tri, cost_tri] = ADMM_tridiag(noisey, F, S, C1, C2, ...
-        beta, xinit, img, niters, 'mu_args', {'fancy_mu34', true, 'test', 'edgeRRapproxcheck'});
+        beta, xinit, img, niters, 'mu_args', {'faNcy_mu34', true, 'test', 'edgeRRapproxcheck'});
 
 figure; subplot(2,2,1); im(xinit); subplot(2,2,2); im(xhat_bs); subplot(2,2,3); im(xhat_P2); subplot(2,2,4); im(xhat_tri)
-calc_NRMSE_over_mask(xhat_bs, xhat_tri, true(nx,ny))
+calc_NRMSE_over_mask(xhat_bs, xhat_tri, true(Nx,Ny))
 
 return
 save('races_results.mat','beta','alph','err','xhat_P2','xsaved_P2','dist_to_xinf_P2','xhat_tri','xsaved_tri','dist_to_xinf_tri','xhat_FP','xsaved_tri','dist_to_xinf_FP','img','noisey','F','S','C1','C2','mu','snr');
