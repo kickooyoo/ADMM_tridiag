c492870ad473fe0f86646084991615ada1e3728d
kickooyoo <kickooyoo@gmail.com>
2016-08-23 14:57:27 -0400

    better downsampled image and factorizable image size

--- inpainting_setup.m
+++ inpainting_setup.m
@@ -9,11 +9,12 @@ betas = [0.02 0.02 0.02 0.02 0.2 0.02 0.02 0.02 0.02 0.2 0.02 0.02 0.02 0.02];
 for ii = 1%[4 6 10]
         fname = fnames{ii};
         xtrue = imread(fname);
-        xtrue = xtrue(1:8:end,1:8:end,:);
+
         if size(xtrue, 3) > 1
                 xtrue = rgb2gray(xtrue);
         end
-        
+        xtrue = double(xtrue);
+        xtrue = downsample2(xtrue, 7);
         
         % make even dimensions
         if mod(size(xtrue, 1), 2) == 1
@@ -22,8 +23,10 @@ for ii = 1%[4 6 10]
         if mod(size(xtrue, 2), 2) == 1
                 xtrue = xtrue(:, 2:end);
         end
-        xtrue = double(xtrue);
+        xtrue = xtrue(:,1:540);
+
         xtrue = xtrue./max(xtrue(:));
+        
         [Nx, Ny] = size(xtrue);
 
 	rng(0);
@@ -66,9 +69,14 @@ for ii = 1%[4 6 10]
 					CHW = CH;
 					CVW = CV;
 					RW = R;
-				end
-				[x(:,:,jj,kk), xsaved, err(:,ii,jj,kk), cost, time(:,ii,jj,kk)] = AL_tridiag_inpaint(y, D, CHW, CVW, ...
-						beta, xinit, xtrue, niters, 'mu', {1, 1, 1}, 'betaw', betaw);
+                                end
+                                if ~isempty(strfind(machine, 'vega'))
+                                        display('mex will not work for vega');
+                                        keyboard;
+                                else
+                                        [x(:,:,jj,kk), xsaved, err(:,ii,jj,kk), cost, time(:,ii,jj,kk)] = AL_tridiag_inpaint(y, D, CHW, CVW, ...
+                                                beta, xinit, xtrue, niters, 'mu', {1, 1, 1}, 'betaw', betaw);
+                                end
 
 				[x_P2(:,:,jj,kk), xsave_P2, err_P2(:,ii,jj,kk), costOrig_P2, time_P2(:,ii,jj,kk)] = AL_P2_inpainting(y, D, RW, ...
 					xinit, niters, beta, xtrue, 'mu',  {1, 1} );
@@ -79,14 +87,14 @@ for ii = 1%[4 6 10]
 		end
         end
 %        best_beta_ndx = find(err_P2(end,ii,:) == min(err_P2(end,ii,:)))
-        [best_beta_ndx, best_betaw_ndx] = find(squeeze(err(end,ii,:,:)) == min(col(err(end,ii,:,:))))
-        [best_circ_beta_ndx, best_circ_betaw_ndx] = find(squeeze(err_circ(end,ii,:,:)) == min(col(err_circ(end,ii,:,:))))
+        [best_beta_ndx, best_betaw_ndx] = find(squeeze(err(end,ii,:,:)) == min(col(err(end,ii,:,:))), 1, 'first')
+        [best_circ_beta_ndx, best_circ_betaw_ndx] = find(squeeze(err_circ(end,ii,:,:)) == min(col(err_circ(end,ii,:,:))), 1, 'first')
         
-        figure; plot(cumsum(time(:,ii,best_beta_ndx, best_betaw_ndx)), err(:,ii,best_beta_ndx, best_betaw_ndx))
-        hold on; plot(cumsum(time_circ(:,ii,best_circ_beta_ndx, best_circ_betaw_ndx)), err_circ(:,ii,best_circ_beta_ndx, best_circ_betaw_ndx), 'r')
+        figure; plot(cumsum(time(:,ii,best_beta_ndx, best_betaw_ndx)), err(:,ii,best_beta_ndx, best_betaw_ndx));
+        hold on; plot(cumsum(time_circ(:,ii,best_circ_beta_ndx, best_circ_betaw_ndx, best_circ_betaw_ndx)), err_circ(:,ii,best_circ_beta_ndx, best_circ_betaw_ndx, best_circ_betaw_ndx), 'r')
         %         keyboard
-        figure; subplot(1,2,1); im(x(:,:,best_beta_ndx)); subplot(1,2,2); im(x(:,:,best_beta_ndx) - xtrue);
-        figure; subplot(1,2,1); im(x_circ(:,:,best_circ_beta_ndx)); subplot(1,2,2); im(x_circ(:,:,best_circ_beta_ndx) - xtrue);
+        figure; subplot(1,2,1); im(x(:,:,best_beta_ndx, best_betaw_ndx)); subplot(1,2,2); im(x(:,:,best_beta_ndx, best_betaw_ndx) - xtrue);
+        figure; subplot(1,2,1); im(x_circ(:,:,best_circ_beta_ndx, best_circ_betaw_ndx)); subplot(1,2,2); im(x_circ(:,:,best_circ_beta_ndx, best_circ_betaw_ndx) - xtrue);
 end
 
 save(sprintf('inpainting_%s', machine(1:3)))
